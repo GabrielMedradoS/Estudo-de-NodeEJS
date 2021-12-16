ebac723da6d46f12561b9fbef5202c4f17bd8397
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-12-16 16:54:40 -0300

    forEach EJS

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const app = express(); //instanciando o express
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("pages/index");
+  const items = [
+    { title: "D", message: "esenvolver aplicaçoes" },
+    { title: "E", message: "JS usa javascript para renderizar HTML" },
+    { title: "M", message: "uito fácil" },
+    { title: "A", message: "mazing" },
+    { title: "I", message: "squece" },
+    { title: "S", message: "ensacional" },
+  ];
+
+  const subtitle =
+    "Uma lingaugem de modelagem para criaçao de paginas HTML utilizando JavaScript";
+
+  res.render("pages/index", { qualitys: items, subtitle: subtitle });
 });
 
 app.get("/sobre", function (req, res) {
